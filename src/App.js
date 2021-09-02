cc29a4461aa44c8903bc481d8e09f31180df0779
Mohammad Usman <usmankhann866@gmail.com>
2021-09-02 20:25:14 +0500

    React Rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
+import { Component } from "react";
+import { div } from 'prelude-ls';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component{
+
+  render(){
+  return(
+    
+    /* 1: JSX expression must have one Parent Element.
+       so the best way to solve it by writing all the child elements in a single parent "div" element
+    
+      2: Every elements must have a closing because its JSX not HTML. For br img we will introduce closing
+         like <br/> <img/> 
+         
+      3: We will write ClassName not Class for css classes because its JSX */    
+    
+   /* whenver i use the variable i use the curly braces "{}" which i aleardy imported */
+  
+   <div className="App"> 
+   
+   <Navbar dark color="primary">
+  <div className="container">
+  <NavbarBrand href="/"> Musa Resturant </NavbarBrand>
+  </div> 
+   </Navbar>
+   </div>
+   
   );
+  }
 }
 
 export default App;
